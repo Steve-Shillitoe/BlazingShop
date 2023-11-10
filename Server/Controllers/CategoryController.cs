fb5e261526ede59c23609943262ef71e1acb0f63
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-11-10 15:36:14 +0000

    Created server-side controller

---
+++ Server/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using BlazingShop.Server.Services.CategoryService;
+using BlazingShop.Shared;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> GetCategories()
+        {
+            return Ok(new List<Category>
+            {
+                new Category{Id=1, Name="Books", Url="books", Icon="book"},
+                new Category{Id=2, Name="Electronics", Url="electronics", Icon="camera-slr"},
+                new Category{Id=3, Name="Video Games", Url="video-games", Icon="aperture"}
+            });
+        }
+    }
+}
